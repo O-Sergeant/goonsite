06f19f443d182151ecafeea9532a0a0b28d0c658
O-Sergeant <90175377+O-Sergeant@users.noreply.github.com>
2023-11-09 21:25:46 +1300

    Update navigateTo.js

--- navigateTo.js
+++ navigateTo.js
@@ -2,6 +2,8 @@ function navigateTo(targetPage) {
     console.log(`Navigating to ${targetPage}`);
     const currentPage = document.querySelector('.page');
     const nextPage = document.querySelector(`#${targetPage.replace('.html', '')}`);
+    console.log('Current Page:', currentPage);
+    console.log('Next Page:', nextPage);
 
     if (currentPage && nextPage) {
         // Apply slide-out class to current page
