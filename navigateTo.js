ba8b70192f4b560995575681545f558f925514b3
O-Sergeant <90175377+O-Sergeant@users.noreply.github.com>
2023-11-09 20:51:10 +1300

    Update navigateTo.js

--- navigateTo.js
+++ navigateTo.js
@@ -1,14 +1,9 @@
 function navigateTo(targetPage) {
-    console.log(`Navigating to ${targetPage}`);
-    
     const currentPage = document.querySelector('.page');
     const nextPage = document.querySelector(`#${targetPage.replace('.html', '')}`);
 
     if (currentPage && nextPage) {
-        // Slide out the current page to the left
         currentPage.style.transform = 'translateX(-100%)';
-
-        // Slide in the next page from the right
         nextPage.style.transform = 'translateX(0)';
     }
 }
