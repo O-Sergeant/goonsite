de5c73b4424cdae790a6dc6460526b0047f486e3
O-Sergeant <90175377+O-Sergeant@users.noreply.github.com>
2023-11-09 21:42:25 +1300

    Update navigateTo.js

--- navigateTo.js
+++ navigateTo.js
@@ -1,9 +1,6 @@
 function navigateTo(targetPage) {
-    console.log(`Navigating to ${targetPage}`);
     const currentPage = document.querySelector('.page');
     const nextPage = document.querySelector(`#${targetPage.replace('.html', '')}`);
-    console.log('Current Page:', currentPage);
-    console.log('Next Page:', nextPage);
 
     if (currentPage && nextPage) {
         currentPage.style.display = 'none';
