63673f6c607f430dffd1627e376ead3ef1d2e419
O-Sergeant <90175377+O-Sergeant@users.noreply.github.com>
2023-11-09 20:53:49 +1300

    Update navigateTo.js

--- navigateTo.js
+++ navigateTo.js
@@ -3,7 +3,16 @@ function navigateTo(targetPage) {
     const nextPage = document.querySelector(`#${targetPage.replace('.html', '')}`);
 
     if (currentPage && nextPage) {
-        currentPage.style.transform = 'translateX(-100%)';
-        nextPage.style.transform = 'translateX(0)';
+        // Add class to current page for sliding out
+        currentPage.classList.add('slide-out');
+
+        // Add class to next page for sliding in
+        nextPage.classList.add('slide-in');
+
+        // Remove classes after the transition duration
+        setTimeout(() => {
+            currentPage.classList.remove('slide-out');
+            nextPage.classList.remove('slide-in');
+        }, 500); // Adjust the timeout to match your transition duration
     }
 }
