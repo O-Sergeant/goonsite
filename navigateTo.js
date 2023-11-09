3a9c54a7114ba06356388a281658f71125182bbd
O-Sergeant <90175377+O-Sergeant@users.noreply.github.com>
2023-11-09 21:31:26 +1300

    Update navigateTo.js

--- navigateTo.js
+++ navigateTo.js
@@ -1,21 +1,9 @@
 function navigateTo(targetPage) {
-    console.log(`Navigating to ${targetPage}`);
     const currentPage = document.querySelector('.page');
     const nextPage = document.querySelector(`#${targetPage.replace('.html', '')}`);
-    console.log('Current Page:', currentPage);
-    console.log('Next Page:', nextPage);
 
     if (currentPage && nextPage) {
-        // Apply slide-out class to current page
-        currentPage.classList.add('slide-out');
-
-        // Apply slide-in class to next page
-        nextPage.classList.add('slide-in');
-
-        // Remove classes after the transition duration
-        setTimeout(() => {
-            currentPage.classList.remove('slide-out');
-            nextPage.classList.remove('slide-in');
-        }, 500); // Adjust the timeout to match your transition duration
+        currentPage.style.display = 'none';
+        nextPage.style.display = 'block';
     }
 }
